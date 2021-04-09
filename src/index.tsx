d44206a46aaed70a41dcb6b1c8f6cd4f4ab073a9
Mark <jardenilmark@gmail.com>
2021-04-09 20:05:54 +0800

    feat: initial commit

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native Init
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import * as React from 'react'
+
+import { View, Text } from 'react-native'
+
+const Root = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default Root
